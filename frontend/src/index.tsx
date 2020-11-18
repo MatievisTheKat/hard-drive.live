485a3d06f5eb12688eea42ff9891aba9c31b0c10
MatievisTheKat <matievisthekat@gmail.com>
2020-11-18 11:41:52 +0200

    move css

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import "./tailwind.output.css";
-import "./index.css";
+import "./assets/tailwind.out.css";
+import "./assets/index.css";
 
 ReactDOM.render(
 	<React.StrictMode>
